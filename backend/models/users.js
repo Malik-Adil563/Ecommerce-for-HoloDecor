050227a23c68da1054ce43f06c9ac7effc9429d4
Malik-Adil563 <madil4329@gmail.com>
2025-04-26 14:20:36 +0500

    Update users.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,38 +1,27 @@
+// backend/models/Products.js
 const mongoose = require('mongoose');
-
 // Connect to the MongoDB cluster and specify the database 'adilm09'
-mongoose.connect('mongodb+srv://adilm09:Camb786@cluster0.kb3vcsh.mongodb.net/ecommerce?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect('mongodb+srv://adilm09:Camb786@cluster0.kb3vcsh.mongodb.net/ecommerce?retryWrites=true&w=majority')
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB products');
   })
   .catch(err => {
     console.error('Error connecting to MongoDB:', err);
   });
 
-// Define the schema for the 'users' collection
-const userSchema = new mongoose.Schema({
-    name:{
-        type : String,
-        required : true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }, 
-    token:{
-        type: String,
-        default: null
-    }
-}, {timestamps: true})
+const userInSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+  rating: {
+    rate: Number,
+    count: Number
+  },
+  productCode: Number,
+  id: Number
+});
 
-const User = mongoose.model("User", userSchema, 'users');
-
-module.exports = User;
+const Products = mongoose.model("Products", userInSchema, 'catProducts');
+module.exports = Products;
