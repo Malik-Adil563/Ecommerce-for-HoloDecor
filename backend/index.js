2ebfb72c5ad85819f48a6b53105ba544e13472b3
Malik-Adil563 <madil4329@gmail.com>
2025-04-26 14:27:00 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: ["http://localhost:3000", "https://holo-decor-ar-frontend.vercel.app/"], // Add your actual frontend URL
     methods: ["GET", "POST"],
-    credentials: true
+    credentials: true,
+    origin: true
 }));
 app.use(cookieParser());
 
