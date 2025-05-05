8055b93e4564fd96880642a15358f66d819fdc5e
Malik-Adil563 <madil4329@gmail.com>
2025-05-06 00:49:38 +0500

    Update users.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,13 +1,5 @@
 // backend/models/Products.js
 const mongoose = require('mongoose');
-// Connect to the MongoDB cluster and specify the database 'adilm09'
-mongoose.connect('mongodb+srv://adilm09:Camb786@cluster0.kb3vcsh.mongodb.net/internee/Users?retryWrites=true&w=majority')
-  .then(() => {
-    console.log('Connected to MongoDB products');
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-  });
 
 const userInSchema = new mongoose.Schema({
   title: String,
