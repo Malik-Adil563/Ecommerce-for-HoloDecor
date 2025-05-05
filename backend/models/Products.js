c991a0e879457040513059615397eb795f10b058
Malik-Adil563 <madil4329@gmail.com>
2025-05-06 01:07:16 +0500

    Update Products.js

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -1,17 +1,35 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-    title: String,
-    price: Number,
-    description: String,
-    category: String,
-    image: String,
-    rating: {
-        rate: Number,
-        count: Number
-    },
-    productCode: Number,
-    id: Number
-});
+// Connect to the MongoDB cluster and specify the database 'adilm09'
+mongoose.connect('mongodb+srv://adilm09:Camb786@cluster0.kb3vcsh.mongodb.net/internee/ecommerce?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to MongoDB products');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-module.exports = mongoose.model("Product", productSchema, 'products');
+// Define the schema for the 'users' collection
+const userSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }, 
+    token:{
+        type: String,
+        default: null
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("User", userSchema, 'users');
+
+module.exports = User;
