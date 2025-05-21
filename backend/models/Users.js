05a05e79fea207af22d53f1a4590d74ab6383c54
Malik-Adil563 <madil4329@gmail.com>
2025-05-21 17:05:47 +0500

    Update and rename users.js to Users.js

---
+++ backend/models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the 'users' collection
+const userSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }, 
+    token:{
+        type: String,
+        default: null
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("User", userSchema, 'users');
+
+module.exports = User;
