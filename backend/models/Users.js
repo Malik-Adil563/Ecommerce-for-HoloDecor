5c891ba9d2814a890c4f69dfe8cb57646f652536
Malik-Adil563 <madil4329@gmail.com>
2025-06-18 17:22:07 +0500

    Update Users.js

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose');
 
-// Define the schema for the 'users' collection
 const userSchema = new mongoose.Schema({
-    name:{
-        type : String,
-        required : true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }, 
-    token:{
-        type: String,
-        default: null
-    }
-}, {timestamps: true})
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  token: {
+    type: String,
+    default: null
+  },
+  resetToken: {
+    type: String
+  },
+  resetTokenExpire: {
+    type: Date
+  }
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema, 'users');
-
 module.exports = User;
