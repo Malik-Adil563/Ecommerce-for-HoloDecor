c00b18812ea3399134b93e4686e8bd39a82b3423
adilm67 <163647730+adilm67@users.noreply.github.com>
2025-06-30 23:33:25 +0500

    initail comment

--- backend/predict.js
+++
@@ -1,9 +0,0 @@
-// predict.js
-const tf = require('@tensorflow/tfjs-node');
-
-async function loadModel() {
-  const model = await tf.loadGraphModel('file://path/to/tfjs_wall_model/model.json');
-  console.log("Model loaded successfully!");
-}
-
-loadModel();
